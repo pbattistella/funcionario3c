80e525192b09123a8d2ebc67d7769d3bad76a3df
pbattistella <battistellaa@gmail.com>
2021-04-12 07:57:27 -0300

    projeto para o 3c do senai

---
+++ src/main/java/br/com/sistema/controller/FuncionarioController.java
@@ -0,0 +1,21 @@
+package br.com.sistema.controller;
+
+import br.com.sistema.service.FuncionarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FuncionarioController {
+
+    @Autowired
+    FuncionarioServiceImpl funcionarioService;
+
+    @GetMapping("/funcionario/list")
+    public String list(Model model){
+        model.addAttribute("funcionarios", funcionarioService.findAll());
+        System.out.println(funcionarioService.findAll());
+        return "funcionario/list";
+    }
+}
